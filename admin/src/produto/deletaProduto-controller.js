be1b1e376911f2ba4f92cceac981735b53b9ab55
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-05-02 02:04:35 -0300

    modificando as Pastas

---
+++ admin/src/produto/deletaProduto-controller.js
@@ -0,0 +1,20 @@
+export function DeletaProduto(section, produtoService) {
+        section.addEventListener('click', async (evento)=> {
+            let ehBotaoDeDeleta = evento.target.className === 'botao__produto--edit botao__produto--excluir'
+              if(ehBotaoDeDeleta ){
+                  try {
+
+                      const linhaProduto = evento.target.closest('[data-id]')
+                      let id = linhaProduto.dataset.id
+                      await produtoService.removeProduto(id)
+                      evento.preventDefault()
+
+                  }
+                  catch(erro){
+                      console.log(erro)
+                      window.location.href="../public/erro.html"
+                  }
+              }
+        })
+        
+    }
